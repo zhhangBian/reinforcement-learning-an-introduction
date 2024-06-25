301a4a65483e79d89f034a6c4041578f40b93852
pigkiller <2317164114@qq.com>
2024-06-25 16:12:05 +0800

    0625 16:10 run chap01

--- chapter01/tic_tac_toe.py
+++ chapter01/tic_tac_toe.py
@@ -1,13 +1,3 @@
-#######################################################################
-# Copyright (C)                                                       #
-# 2016 - 2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)           #
-# 2016 Jan Hakenberg(jan.hakenberg@gmail.com)                         #
-# 2016 Tian Jun(tianjun.cpp@gmail.com)                                #
-# 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
-# Permission given to modify the code as long as you keep this        #
-# declaration at the top                                              #
-#######################################################################
-
 import numpy as np
 import pickle
 
